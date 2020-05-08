372de8dddcb9482205f55a6fafd8b4aa3cc48f1d
Mirza Husic <mirza.husic@fet.ba>
2020-05-08 10:27:34 +0200

    add new item

---
+++ src/com/todolist/DialogController.java
@@ -0,0 +1,28 @@
+package com.todolist;
+
+import com.todolist.datamodel.TodoData;
+import com.todolist.datamodel.TodoItem;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+import java.time.LocalDate;
+
+public class DialogController {
+    @FXML
+    private TextField shortDescriptionFiled;
+    @FXML
+    private TextArea detailsArea;
+    @FXML
+    private DatePicker deadlinePicker;
+
+    public TodoItem processResults(){
+        String shortDescription = shortDescriptionFiled.getText().trim();
+        String details = detailsArea.getText().trim();
+        LocalDate deadLineValue = deadlinePicker.getValue();
+        TodoItem newItem=new TodoItem(shortDescription,details,deadLineValue);
+        TodoData.getInstance().addTodoItem(newItem);
+        return newItem;
+    }
+}
